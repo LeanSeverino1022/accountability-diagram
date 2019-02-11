4d77e0b976b967764a06e45f828cffa12a0314a7
Puyih Severino <yihseverino@gmail.com>
2019-02-11 12:46:35 +0800

    format code, spacin only

--- js/data/data.js
+++ js/data/data.js
@@ -1,71 +1,64 @@
-
-window.seatData =
-    {
-        "title": "Integrator",
-        "seatOwnerId": "1",
-        "seatOwner": "Patrick Bilbao",
-        "username": "patbilbao",
-        "description": "<ul> <li>Lead, Manage  Accountability (LMA)</li>  <li>profit & Loss/Business Plan</li>  <li>Remove Obstacles & Barriers</li>  <li>Special Projects</li> <li>More on Logic</li></ul>",
-        "children":
-            [
+window.seatData = {
+    title: 'Integrator',
+    seatOwnerId: '1',
+    seatOwner: 'Patrick Bilbao',
+    username: 'patbilbao',
+    description:
+        '<ul> <li>Lead, Manage  Accountability (LMA)</li>  <li>profit & Loss/Business Plan</li>  <li>Remove Obstacles & Barriers</li>  <li>Special Projects</li> <li>More on Logic</li></ul>',
+    children: [
+        {
+            title: 'SALES',
+            seatOwnerId: '2',
+            seatOwner: 'Jason Santibanez',
+            username: 'jSan',
+            description:
+                '<ul> <li>LMA</li>  <li>Lead Generation</li>  <li>Firecasting</li>  <li>Pricing</li> </ul>',
+            children: [
                 {
-                    "title": "SALES",
-                    "seatOwnerId": "2",
-                    "seatOwner": "Jason Santibanez",
-                    "username": "jSan",
-                    "description": "<ul> <li>LMA</li>  <li>Lead Generation</li>  <li>Firecasting</li>  <li>Pricing</li> </ul>",
-                    "children": [
-                        {
-                            "title": "Marketing",
-                            "seatOwnerId": "14",
-                            "seatOwner": "Jessica Soho",
-                            "username": "jessoho",
-                            "description": "",
-                        }
-                    ]                    
+                    title: 'Marketing',
+                    seatOwnerId: '14',
+                    seatOwner: 'Jessica Soho',
+                    username: 'jessoho',
+                    description: ''
+                }
+            ]
+        },
+        {
+            title: 'OPERATIONS',
+            seatOwnerId: '12',
+            seatOwner: 'Jack Frost',
+            username: 'icecold',
+            description:
+                '<ul> <li>LMA</li>  <li>Service Delivery</li>  <li>Fleet</li>  <li>Facilities</li></ul>',
+            children: [
+                {
+                    title: 'Maintenance',
+                    seatOwnerId: '28',
+                    seatOwner: 'Bryle Stephenson',
+                    username: 'realStephenson',
+                    description: ''
                 },
                 {
-                    "title": "OPERATIONS",
-                    "seatOwnerId": "12",
-                    "seatOwner": "Jack Frost",
-                    "username": "icecold",
-                    "description": "<ul> <li>LMA</li>  <li>Service Delivery</li>  <li>Fleet</li>  <li>Facilities</li></ul>",
-                    "children": [
-                        {
-                            "title": "Maintenance",
-                            "seatOwnerId": "28",
-                            "seatOwner": "Bryle Stephenson",
-                            "username": "realStephenson",
-                            "description": "",
-                        },
-                        {
-                            "title": "Compliance",
-                            "seatOwnerId": "24",
-                            "seatOwner": "Bean Bryant",
-                            "username": "beanMamba",
-                            "description": "",
-                        }
-                    ]
+                    title: 'Compliance',
+                    seatOwnerId: '24',
+                    seatOwner: 'Bean Bryant',
+                    username: 'beanMamba',
+                    description: ''
                 }
-            ],
+            ]
+        }
+    ]
+};
 
-
-    };
-
-
-window.seatData2 =
-    
 //sample teammates data for knockout select
 window.teammates = [
-
-    { "fullName": "Patrick Bilbao", "login": "patbilbao  ", "id": 1 },
-    { "fullName": "Jason Santibanez", "login": "jSan ", "id": 2 },
-    { "fullName": "Jessica Soho", "login": "jessoho ", "id": 14 },
-    { "fullName": "Jack Frost", "login": "icecold ", "id": 12 },
-    { "fullName": "Bryle Stephenson", "login": "realStephenson ", "id": 28 },
-    { "fullName": "Bean Bryant", "login": "beanMamba", "id": 24  },
-    { "fullName": "Charles Livingston", "login": "charlingston", "id": 41 },
-    { "fullName": "Mark Granger", "login": "granger101", "id": 53 },
-    { "fullName": "Dave Stu", "login": "devilsboy", "id": 54 },
+    { fullName: 'Patrick Bilbao', login: 'patbilbao  ', id: 1 },
+    { fullName: 'Jason Santibanez', login: 'jSan ', id: 2 },
+    { fullName: 'Jessica Soho', login: 'jessoho ', id: 14 },
+    { fullName: 'Jack Frost', login: 'icecold ', id: 12 },
+    { fullName: 'Bryle Stephenson', login: 'realStephenson ', id: 28 },
+    { fullName: 'Bean Bryant', login: 'beanMamba', id: 24 },
+    { fullName: 'Charles Livingston', login: 'charlingston', id: 41 },
+    { fullName: 'Mark Granger', login: 'granger101', id: 53 },
+    { fullName: 'Dave Stu', login: 'devilsboy', id: 54 }
 ];
-
