a0589a5f1da8019336391d15285efbb67a91f0ca
Puyih Severino <yihseverino@gmail.com>
2019-02-11 12:44:34 +0800

    update data and text font-weight

--- js/data/data.js
+++ js/data/data.js
@@ -17,16 +17,16 @@ window.seatData =
                     "children": [
                         {
                             "title": "Marketing",
-                            "seatOwnerId": "2",
+                            "seatOwnerId": "14",
                             "seatOwner": "Jessica Soho",
-                            "username": "amy@dishcrawl.com",
+                            "username": "jessoho",
                             "description": "",
                         }
                     ]                    
                 },
                 {
                     "title": "OPERATIONS",
-                    "seatOwnerId": "2",
+                    "seatOwnerId": "12",
                     "seatOwner": "Jack Frost",
                     "username": "icecold",
                     "description": "<ul> <li>LMA</li>  <li>Service Delivery</li>  <li>Fleet</li>  <li>Facilities</li></ul>",
@@ -35,12 +35,12 @@ window.seatData =
                             "title": "Maintenance",
                             "seatOwnerId": "28",
                             "seatOwner": "Bryle Stephenson",
-                            "username": "leanseverino",
+                            "username": "realStephenson",
                             "description": "",
                         },
                         {
                             "title": "Compliance",
-                            "seatOwnerId": "14",
+                            "seatOwnerId": "24",
                             "seatOwner": "Bean Bryant",
                             "username": "beanMamba",
                             "description": "",
@@ -54,70 +54,18 @@ window.seatData =
 
 
 window.seatData2 =
-    {
-        "title": "Visionary",
-        "seatOwnerId": "98",
-        "seatOwner": "Sushil Simple",
-        "username": "sushilsimple",
-        "description": "<ul><li>20 Ideas</li>  <li>Creativity/Problem Solving</li>  <li>Big Relationships</li>  <li>Culture</li>  <li>R&D</li>  <li>More on Emotion</li></ul>",
-        "children": [
-            {
-                "title": "Integratorzz",
-                "seatOwnerId": "4",
-                "seatOwner": "Docs mine",
-                "username": "Docsmine",
-                "description": "<ul> <li>Lead, Manage  Accountability (LMA)</li>  <li>profit & Loss/Business Plan</li>  <li>Remove Obstacles & Barriers</li>  <li>Special Projects</li> <li>More on Logic</li></ul>",
-                "children": [
-                    {
-                        "title": "Operationszz",
-                        "seatOwnerId": "28",
-                        "seatOwner": "Amy Peterson",
-                        "username": "amy@dishcrawl.com",
-                        "description": "<ul><li>LMA</li>  <li>Sales/Revenue Goal</li>  <li>Selling</li>  <li>Marketting</li>  <li>Sales & Marketting Process</li>  </ul>",
-                    },
-                    {
-                        "title": "Sales/Mktgzz",
-                        "seatOwnerId": "14",
-                        "seatOwner": "Nicole Mossman",
-                        "username": "NicoleMossman",
-                        "description": "<ul><li>LMA</li>  <li>Sales/Revenue Goal</li>  <li>Selling</li>  <li>Marketting</li>  <li>Sales & Marketting Process</li>  </ul>",
-                    },
-                    {
-                        "title": "Financezz",
-                        "seatOwnerId": "6",
-                        "seatOwner": "Glen Tenorio",
-                        "username": "GlenTenorio",
-                        "description": "<ul><li>LMA</li>  <li>Budgeting</li>  <li>Reporting</li>  <li>HR/Admin</li>  <li>IT</li>  <li>Office Management</li></ul>",
-                    }
-                ]
-            },
-        ],
-
-
-
-
-    };
+    
 //sample teammates data for knockout select
 window.teammates = [
 
-    { "fullName": "Docs mine", "login": "Docsmine", "id": 4 },
-    { "fullName": "Glen Tenorio", "login": "GlenTenorio", "id": 6 },
-    { "fullName": "Joener Preagola", "login": "JoenerPreagola", "id": 8 },
-    { "fullName": "Kapil Garg", "login": "Kapil", "id": 9 },
-    { "fullName": "Laris Orman", "login": "Laris", "id": 10 },
-    { "fullName": "Marina Tarashevska", "login": "Marina T", "id": 11 },
-    { "fullName": "Max Menke", "login": "Max", "id": 12 },
-    { "fullName": "Nick Schanbaum", "login": "NickSchanbaum", "id": 13 },
-    { "fullName": "Nicole Mossman", "login": "NicoleMossman", "id": 14 },
-    { "fullName": "Patrick Angodung", "login": "PatrickAngodung", "id": 15 },
-    { "fullName": "Aditya Agustyana", "login": "aditya", "id": 24 },
-    { "fullName": "Amy Peterson", "login": "amy@dishcrawl.com", "id": 28 },
-    { "fullName": "Bobby Simpson", "login": "bobbysimpson", "id": 35 },
-    { "fullName": "Sam Chae", "login": "cleverbirds", "id": 39 },
-    { "fullName": "Catherin McLaughlin", "login": "cmclaughlin76", "id": 40 },
-    { "fullName": "David Livingston", "login": "davelivingston", "id": 41 },
-    { "fullName": "Hermione Granger", "login": "hermione", "id": 53 },
-    { "fullName": "Indra Setiawan", "login": "indra", "id": 54 },
-    { "fullName": "Sushil Simple", "login": "sushilsimple", "id": 98 },
+    { "fullName": "Patrick Bilbao", "login": "patbilbao  ", "id": 1 },
+    { "fullName": "Jason Santibanez", "login": "jSan ", "id": 2 },
+    { "fullName": "Jessica Soho", "login": "jessoho ", "id": 14 },
+    { "fullName": "Jack Frost", "login": "icecold ", "id": 12 },
+    { "fullName": "Bryle Stephenson", "login": "realStephenson ", "id": 28 },
+    { "fullName": "Bean Bryant", "login": "beanMamba", "id": 24  },
+    { "fullName": "Charles Livingston", "login": "charlingston", "id": 41 },
+    { "fullName": "Mark Granger", "login": "granger101", "id": 53 },
+    { "fullName": "Dave Stu", "login": "devilsboy", "id": 54 },
 ];
 
